4109a4dbf40d3af297c4f9ccd9c26319417129f1
Scott Robertson <srobertson@codeit.com>
2013-04-23 08:32:52 -0700

    feature enhancements * keep a list of feature names * extract host names * log errors

--- features.py
+++ features.py
@@ -1,32 +1,52 @@
 import codd
 print codd.__file__
+from urlparse import urlparse
+
 def size(doc):
-  """Returns size:xxx"""
-  yield 'size', len(doc.payload)
+  """Returns size of the payload"""
+  yield 'size', len(doc['payload'])
+
+def host(doc):
+  """Returns the host for the given doc"""
+  yield "host", urlparse(doc['url']).hostname
 
 
 def content_type(doc):
   """Returns contexnt_type:xxx"""
-  return 'content_type', doc.get('content_type')
+  yield 'content_type', doc.get('content_type')
 
-def scripts(doc):
-  for element in doc.find_all('script'):
-    yield 'script', element.get('src')
+def script(doc):
+  if doc.has_key('html'):
+    for element in doc['html'].find_all('script'):
+      src = element.get('src')
+      if src:
+        yield 'script', src
 
-def links(doc):
-  for element in doc.find_all('a'):
-    yield 'links', element.get('href')
+def link(doc):
+  if doc.has_key('html'):
+    for element in doc['html'].find_all('a'):
+      link =  element.get('href')
+      if link:
+        yield 'link', link
 
-features = codd.parallel(
+methods = [
   size,
+  host,
   content_type,
-  scripts,
-  links
-)
+  script,
+  link
+]
+
+feature_names = [m.__name__ for m in methods]
+features      = codd.parallel(*methods)
 
 def docid(params):
-  doc_count = params['doc_counter'] = params.get('doc_count',0)
-  return (doc_count * params['worker_count']) + params['worker_id']
+  #doc_count = params['doc_counter'] = params.get('doc_count',0)
+  #return (doc_count * params['worker_count']) + params['worker_id']
+  if not hasattr(params, 'doc_count'):
+    params.doc_count = 0
+  params.doc_count += 1
+  return params.doc_count
 
 def extractor(doc, params):
   doc_id = docid(params)
@@ -34,4 +54,7 @@ def extractor(doc, params):
   yield "doc_id:{}".format(doc_id), doc['url']
 
   for feature, value in features(doc):
-    yield "%s:%s" % (k,v), doc_id 
+    try:
+      yield str("%s:%s" % (feature,value)), doc_id
+    except Exception, e:
+      yield "error:{}".format(e), doc_id
