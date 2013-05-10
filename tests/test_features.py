45dd504f40d5094d41fe007ed02659dd21f9e242
Scott Robertson <srobertson@codeit.com>
2013-05-10 15:49:05 -0700

    user urlnorm to normalize urls

--- tests/test_features.py
+++ tests/test_features.py
@@ -5,7 +5,12 @@ from . import TestCase
 
 import features
 
-
+def doc(content, url="http://example.com/folder1/somedoc.html"):
+  return dict(
+    url = url,
+    dom = bs4.BeautifulSoup(content),
+    payload = content
+  )
 
 class TestFeatureExtractors(TestCase):
 
@@ -13,30 +18,68 @@ class TestFeatureExtractors(TestCase):
     content =  """
     <html>
     <body>
-      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-      <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js">
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      <script src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js">
     </body>
     </html>"""
-    doc = bs4.BeautifulSoup(content)
- 
-    doc_features =  list(features.scripts(doc))
+
+    doc_features =  list(features.script(doc(content)))
     eq_(
       doc_features,
-      [('script', u'//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'), 
-      ('script', u'//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js')]
+      [('script', u'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'), 
+      ('script', u'http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js')]
     )
 
   def test_links(self):
-    doc = bs4.BeautifulSoup("""
+    content = """
     <html>
     <body>
       <a href="http://blah.com">blah</a>
+      <a href="../relative/./link">Hi mom</a>
     </body>
     </html>
-    """)
-    doc_features = list(features.links(doc))
-    eq_(
+    """
+    f = features.outbound()
+    doc_features = list(f(doc(content)))
+    self.assertSequenceEqual(
       doc_features,
-      [('links', u'http://blah.com')]
+      [
+        ('link_to', 'blah.com'),
+      ]
     )
-    
+
+  def test_tags(self):
+    content = """
+    <html>
+    <body>
+      <a href="http://blah.com">blah</a>
+      <a href="../relative/./link">Hi mom</a>
+      <div id="1">
+        <span></span>
+        <div>
+          <ul>
+           <li>One</li>
+           <li>Two</li>
+          </ul>
+        </div>
+      </div>
+    </body>
+    </html>
+    """
+    doc_features = list(features.tag(doc(content)))
+    self.assertSequenceEqual(
+      doc_features,
+      [
+        ('tag', 'html'),
+        ('tag', 'html>body'),
+        ('tag', 'html>body>a'),
+        ('tag', 'html>body>a'),
+        ('tag', 'html>body>div'),
+        ('tag', 'html>body>div>span'),
+        ('tag', 'html>body>div>div'),
+        ('tag', 'html>body>div>div>ul'),
+        ('tag', 'html>body>div>div>ul>li'),
+        ('tag', 'html>body>div>div>ul>li')
+      ]
+    )
+   
