c18cd5cbf60de7b05efbe2c57ab924723edde01f
Scott Robertson <srobertson@codeit.com>
2013-07-11 13:46:24 -0700

    tests no longer rely on fixture data in the repo

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,5 @@
 import unittest
+from unittest import TestCase
 import boto
 import os
 import bs4
-
-class TestCase(unittest.TestCase):
-  def setUp(self):
-    # download a sample file
-    self.doc = bs4.BeautifulSoup(open(self.docs()[0]))
-
-  def docs(self):
-    dirname = os.path.dirname(__file__)
-    doc_path = os.path.join(dirname, 'fixtures', 'html')
-    return [ os.path.join(doc_path, doc) for doc in os.listdir(doc_path)]
