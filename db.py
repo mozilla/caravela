08b0d308f6dfb512daafbd9c58f4ade63e6eba3e
Scott Robertson <srobertson@codeit.com>
2013-08-27 14:56:32 -0700

    Add underlying arc file as a table.

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ from splicer import DataSet, Relation
 from splicer.servers.file_server import FileServer
 
 import splicer_console
+import splicer_arc
 from splicer_discodb import DiscoDBServer
 
 
@@ -15,18 +16,54 @@ def init(**tables):
 
   dataset = DataSet()
   dataset.add_server(DiscoDBServer(**tables))
+
+  dataset.add_server(FileServer(
+    arc=dict(
+      root_dir=join(dirname(__file__), 'data'),
+      pattern="sample.arc.gz",
+      decode="application/x-arc"
+    )
+  ))
+
+
   dataset.add_server(FileServer(
     top_sites=dict(
       root_dir=join(dirname(__file__), 'data'),
       pattern="alexa-top1m-{date}.csv",
-      decode="auto"
+      decode="auto",
+      schema=dict(
+        fields=[
+          dict(name="date", type="DATE"),
+          dict(name="rank", type="STRING"),
+          dict(name="site", type="STRING")
+        ]
+      )
     )
   ))
 
-  dataset.select(
-    'date, column_0 as rank, column_1 as site'
-  ).frm('top_sites').limit(10).create_view('top_10')
+  dataset.frm('top_sites').limit(10).create_view('top_10')
 
+  dataset.create_view(
+    'outbound_links',
+    "select link_to, count() "
+    "from flatten(docs, 'link_to') "
+    "group by link_to order by count desc"
+  )
+
+  dataset.create_view(
+    'scripts',
+    "select scripts as script, count() "
+    "from flatten(docs, 'scripts') "
+    "group by script order by count desc"
+  )
+
+  dataset.create_view(
+    'servers',
+    "select headers_value as server_name, count() " 
+    "from flatten(docs, 'headers') " 
+    "where headers_name = 'Server' "
+    "group by server_name order by count desc"
+  )
 
   return dataset
 
