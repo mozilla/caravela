ca6c0e661b68400fb8ed4832d4f1aaf250588962
Scott Robertson <srobertson@codeit.com>
2013-09-29 10:38:15 -0700

    New query state is independent of selected query state

--- web/static/js/controllers/query_controller.js
+++ web/static/js/controllers/query_controller.js
@@ -1,5 +1,12 @@
-App.QueryController = Ember.ObjectController.extend({
+
+App.QueryController = Ember.Table.TableController.extend({
   needs: ['application', 'insightTable'],
+
+  hasHeader: true,
+  hasFooter: false,
+  numFixedColumns: 0,
+  //headerHeight: 30,
+  rowHeight: 30,
 
   query: "",
 
@@ -10,6 +17,48 @@ App.QueryController = Ember.ObjectController.extend({
 
 
   saveDisabled: Ember.computed.not('isDirty'),
+
+  //bodyContent: Em.computed.alias("records"),
+
+  bodyContent:  function(){
+    return Ember.Table.RowArrayProxy.create({
+      tableRowClass: Ember.Table.Row,
+      content: this.get('records')
+    });
+  }.property('records'),
+
+  isDirty: Em.computed.alias("model.isDirty"),
+  name: Em.computed.alias("model.name"),
+  statement: Em.computed.alias("model.statement"),
+  insight: Em.computed.alias("model.insight"),
+
+
+  columns: function(){
+
+    var schema = this.get("schema");//Em.get("App.query.schema");
+    // TODO/BUG: this method fires off after sorting is complete
+    // which resets the state of sortAscending back to true which
+    if (!schema){
+      console.log('columns!!!!!, no schema')
+      return [];
+    }else{
+      console.log('columns!!!!!,with schema')
+      return  schema.map(function(name,index){
+        return Ember.Table.ColumnDefinition.create({
+          columnWidth: 220,
+          headerCellName: name,
+          contentPath: name,
+
+          /*getCellContent: function(row){
+            return row[name]
+          },*/
+          sortAscending: true,
+          headerCellViewClass: 'App.HeaderTreeCell'
+        });
+      });
+    }
+
+  }.property("schema.@each"),
 
 
 
@@ -87,10 +136,21 @@ App.QueryController = Ember.ObjectController.extend({
 
 });
 
+
+/* QueriesNewController defined here because it
+   inherits from QueryController 
+*/
 App.QueriesNewController = App.QueryController.extend({
   needs: ['insightTable'],
   name: null,
   statement: "",
+
+  schema: Em.ArrayProxy.create({content:[]}),
+  records: Em.ArrayProxy.create({content:[]}),
+
+  insight: function(){
+    return this.get('store').find('insight','temp');
+  }.property(),
 
   canSave: function(){
     return  Boolean(this.get('name') && this.get('statement'));
