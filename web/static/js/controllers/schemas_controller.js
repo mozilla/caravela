c3c90ad6c0c5320641c34d8b9743070ac75997c2
Scott Robertson <srobertson@codeit.com>
2013-09-30 13:56:08 -0700

    Schema page uses queries.new route for displaying the query. And query state is reset when the route is entered and left.

--- web/static/js/controllers/schemas_controller.js
+++ web/static/js/controllers/schemas_controller.js
@@ -1,15 +1,16 @@
 App.SchemasController = Ember.ArrayController.extend({
-  //needs: ['query'],
+  needs: ['queries_new'],
 
   actions:{
     toggleSchema: function(schema){
       schema.toggleProperty('showSchema');
     },
+
     query: function(schema){
       var stmt = "select * from %@ limit 10".fmt(schema.get('name'));
-      var query_controller = this.get('controllers.query');
-      query_controller.set('query', stmt);
-      this.transitionToRoute('query');
+      var query_controller = this.get('controllers.queries_new');
+      query_controller.set('statement', stmt);
+      this.transitionToRoute('queries.new');
     }
   }
 });
--- web/static/js/routes/queries_new_route.js
+++ web/static/js/routes/queries_new_route.js
@@ -1,4 +1,23 @@
 App.QueriesNewRoute = Em.Route.extend({
+  activate: function(){
+    var controller = this.controllerFor('queries.new')
+    var statement = controller.get('statement');
+    if(statement){
+      controller.send('execute')
+    }
+    return true;
+  },
+  deactivate: function(){
+    var controller = this.controllerFor('queries.new');
+    controller.setProperties({
+      name:null,
+      statement: ""
+    });
+    controller.get('records').clear();
+    controller.get('schema').clear();
+    return true;
+  },
+
 
   renderTemplate: function(){
     this.render('query',
